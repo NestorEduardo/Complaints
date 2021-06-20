f6c7f124a75c3be5c6e6fc70ea99016f23164deb
Nestor De La Cruz <ndelacruz@evopointsolution.com>
2021-06-19 21:12:32 -0400

    setup dbcontext

---
+++ Complaints.Core/Models/BaseEntity.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Complaints.Core.Models
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public bool IsActive { get; set; } = true;
+        public DateTime? DeletedAt { get; set; }
+        public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
--- Complaints.Web/Startup.cs
+++ Complaints.Web/Startup.cs
@@ -1,24 +1,24 @@
+using Complaints.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Complaints.Web
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = Configuration["Data:localhost:ConnectionString"];
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString, builder => builder.MigrationsAssembly(typeof(Startup).Assembly.FullName)));
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
